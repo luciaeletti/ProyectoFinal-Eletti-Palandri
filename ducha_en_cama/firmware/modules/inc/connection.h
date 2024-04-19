db815e1043025d4f7e57a0a33ab5242d081a6410
luciaeletti <lucia.eletti@uner.edu.ar>
2024-04-19 11:43:27 -0300

    Se cargan avances en el firmware: wifi y mqtt

---
+++ ducha_en_cama/firmware/modules/inc/connection.h
@@ -0,0 +1,32 @@
+/*
+ * Connection.h
+ *
+ *  Created on: 15/04/2024
+ *      Author: Prototipado
+ */
+#ifndef CONNECTION_H
+#define CONNECTION_H
+/*==================[inclusions]=============================================*/
+#include <string.h>
+#include <stdio.h>
+#include "conditions.h"
+#include "general.h"
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions declaration]=========================*/
+void vConnectionTask(void *pvParameters);
+
+
+
+
+
+
+
+
+#endif   #ifndef CONNECTION_H
